0bf00cf37bf0d8e08f26d38c0965708ccb14bbe8
screat100 <61044260+screat100@users.noreply.github.com>
2020-06-18 01:39:42 +0900

    강화 ui에서 랜덤 리스트 적용
    - LivingEntity에 관련 변수 저장함

--- PROJECT-SS/Assets/Creat/Src/Upgrade.cs
+++ PROJECT-SS/Assets/Creat/Src/Upgrade.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Upgrade : MonoBehaviour
 {
+    // ui
     private GameObject UpgradeUI;
     private GameObject UpgradeUIPanel;
 
+    // ui 열고 닫음에 관여하는 변수
     static bool isClosed = false;
     static bool flag = false;
 
+    // 선택된 번호 
+    LivingEntity livingEntity;
+
+
+
     void Start()
     {
+        livingEntity = GameObject.FindWithTag("Player").GetComponent<LivingEntity>();
         UpgradeUI = GameObject.Find("Upgrade").transform.Find("upgrade_base").gameObject;
         UpgradeUIPanel = GameObject.Find("Upgrade").transform.Find("upgrade_panel").gameObject;
     }
@@ -32,24 +40,33 @@ public class Upgrade : MonoBehaviour
                     OpenUpgradeUI();
                 }
             }
-
-
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         transform.GetComponent<Outline>().enabled = false;
         CloseUpgradeUI();
     }
+
+    // open
     private void OpenUpgradeUI()
     {
         UpgradeUI.SetActive(true);
         UpgradeUIPanel.SetActive(true);
+        livingEntity.listIcon1[livingEntity.upgradeChoiced1].SetActive(true);
+        livingEntity.listIcon2[livingEntity.upgradeChoiced2].SetActive(true);
+        livingEntity.listIcon3[livingEntity.upgradeChoiced3].SetActive(true);
     }
+
+    // close
     private void CloseUpgradeUI()
     {
         UpgradeUI.SetActive(false);
         UpgradeUIPanel.SetActive(false);
+        livingEntity.listIcon1[livingEntity.upgradeChoiced1].SetActive(false);
+        livingEntity.listIcon2[livingEntity.upgradeChoiced2].SetActive(false);
+        livingEntity.listIcon3[livingEntity.upgradeChoiced3].SetActive(false);
     }
 
 
