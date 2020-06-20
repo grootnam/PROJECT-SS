31ebfe2746a469227ec8f41114d10df8aa423db1
hhyungju <96fbgudwn@naver.com>
2020-06-20 17:42:59 +0900

    오류 다 잡았당

---
+++ PROJECT-SS/Assets/Creat/Src/EnhanceBoard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnhanceBoard : MonoBehaviour
+{
+    private bool isOpen;
+
+    [SerializeField]
+    GameObject enhanceBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            isOpen = !isOpen;
+            if (isOpen)
+            {
+                enhanceBoard.SetActive(true);
+            }
+            else
+            {
+                enhanceBoard.SetActive(false);
+            }
+        }
+    }
+}
